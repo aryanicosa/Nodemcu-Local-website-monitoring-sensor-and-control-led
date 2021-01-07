173b7830b1858bb7a8b527f30c4b96473af6cd0a
JE TECH <69202854+aryanicosa@users.noreply.github.com>
2021-01-08 01:43:13 +0700

    Add files via upload

---
+++ web code/ambilDataSensor.php
@@ -0,0 +1,12 @@
+<?php 
+
+	//koneksi
+	require 'function.php';
+
+    //tampilkan dari database
+    $query = mysqli_query ($conn, "SELECT sensor FROM sensor WHERE id = 1");
+    $result = mysqli_fetch_row($query);
+    
+    echo $result[0];
+
+	?>
